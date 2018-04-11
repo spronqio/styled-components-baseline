1d5357343d7db43335a87ed6cb08509c7a61bd5a
JP Wesselink <jpwesselink@gmail.com>
2018-04-11 15:16:13 +0200

    Updated docs

--- packages/styled-components-baseline-examples/pages/index.js
+++ packages/styled-components-baseline-examples/pages/index.js
@@ -4,4 +4,4 @@ import React from 'react';
 import { Heading1 } from '@panacea/styled-components-baseline/Typography';
 
 console.log(12123, Heading1);
-export default () => <div>Welcome to next.js!1122</div>
+export default () => <div>Welcome to next.js!123123123123123</div>
